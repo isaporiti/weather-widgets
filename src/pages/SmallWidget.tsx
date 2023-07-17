46d6e3208224aafc3bfedc842e47e889aca6b141
Ignacio Saporiti <ignacio@saporiti.dev>
2023-07-17 13:23:46 +0200

    uses Weather in small widget

--- src/pages/SmallWidget.tsx
+++ src/pages/SmallWidget.tsx
@@ -1,14 +1,46 @@
 import styles from '@/styles/Widget.module.css'
+import { Weather, WeatherType } from '@/types/Weather'
 
-export function SmallWidget() {
+export default function SmallWidget({weather}: {weather: Weather}): JSX.Element {
   return <main className={`${styles.widget} ${styles.widget__small} ${styles.clear_sky}`}>
     <section>
-      <div className={styles.widget__city_name}>Barcelona</div>
-      <div className={styles.widget__temperature}>22°</div>
+      <div className={styles.widget__city_name}>{weather.city}</div>
+      <div className={styles.widget__temperature}>{formatTemperature(weather.temperature.current)}</div>
     </section>
     <section>
-      <div>☀️</div>
-      <span className={styles.widget__description}>Severe high temperature warning</span>
+      <Icon weatherType={weather.type} />
+      <Description description={weather.description} />
+      <section className={styles.widget__high_low}>
+        <span>H:{formatTemperature(weather.temperature.high)}</span>
+        <span>L:{formatTemperature(weather.temperature.low)}</span>
+      </section>
     </section>
   </main>
 }
+
+function formatTemperature(temperature: number): string {
+  return `${temperature}°`
+}
+
+function Icon({weatherType}: {weatherType: WeatherType}): JSX.Element {
+  const icon = getWeatherIcon(weatherType)
+  return <>{icon}</>
+}
+
+function Description({description}: {description?: string}): JSX.Element | undefined {
+  if (!description) return undefined;
+
+  return <span className={styles.widget__description}>{description}</span>
+}
+
+function getWeatherIcon(weatherType: WeatherType): string {
+  switch(weatherType) {
+    case('sunny'): return '☀️'
+    case('partly_cloudy'): return '🌤️'
+    case('cloudy'): return '☁️'
+    case('rain'): return '🌧️'
+    case('snow'): return '🌨️'
+    default: return '☀️'
+  }
+}
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,26 +2,36 @@ import { useState } from 'react';
 import { Inter } from 'next/font/google'
 import SizeSelector from './SizeSelector'
 import { WidgetSize } from '@/types/WidgetSize';
-import { SmallWidget } from './SmallWidget';
-import { MediumWidget } from './MediumWidget';
-import { LargeWidget } from './LargeWidget';
+import SmallWidget from './SmallWidget';
+import MediumWidget from './MediumWidget';
+import LargeWidget from './LargeWidget';
 import styles from '@/styles/Home.module.css'
+import { Weather } from '@/types/Weather';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [widgetSize, setWidgetSize] = useState<WidgetSize>('S');
+  const weather: Weather = {
+    city: 'Barcelona',
+    type: 'sunny',
+    temperature: {
+      current: 28,
+      high: 31,
+      low: 24
+    }
+  }
 
   return (
     <main className={`${styles.main} ${inter.className}`}>
-      <Widget size={widgetSize} />
+      <Widget size={widgetSize} weather={weather} />
       <SizeSelector widgetSize={widgetSize} setWidgetSize={setWidgetSize} />
     </main>
   )
 }
 
-function Widget({ size }: { size: WidgetSize }) {
-  if (size === 'S') return <SmallWidget />
+function Widget({ size, weather }: { size: WidgetSize, weather: Weather }) {
+  if (size === 'S') return <SmallWidget weather={weather} />
   if (size === 'M') return <MediumWidget />
   return <LargeWidget />
 }
